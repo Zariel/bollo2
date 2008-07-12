45edfb1848b1aab3a8f643173be8476e6677b139
Zariel <c.bannister@gmail.com>
2008-07-12 21:31:10 +0100

    Add config options

--- modules/duration.lua
+++ modules/duration.lua
@@ -6,7 +6,7 @@ function duration:OnInitialize()
 	local defaults = {
 		profile = {
 			["Description"] = "Show buff and debuff durations",
-			["Point"] = "TOP",
+			["point"] = "TOP",
 			["font"] = STANDARD_TEXT_FONT,
 			["fontSize"] = 9,
 			["fontStyle"] = "OUTLINE",
@@ -17,59 +17,144 @@ function duration:OnInitialize()
 	self.db =  bollo.db:RegisterNamespace("Bollo-Duration", defaults)
 
 	bollo.RegisterCallback(duration, "PostCreateIcon")
+		if not self.options then
+		self.options = {
+			name = "Duration",
+			type = "group",
+			args = {
+				general = {
+					guiInline = true,
+					name = "General Settings",
+					type = "group",
+					order = 1,
+					set = function(info, val)
+						local key = info[# info]
+						self.db.profile[key] = val
+						self:UpdateDisplay()
+					end,
+					get = function(info)
+						local key = info[# info]
+						return self.db.profile[key]
+					end,
+					args = {
+						info = {
+							name = "Display Duration left of buffs",
+							type = "description",
+							order = 1,
+						},
+						enable = {
+							name = "Enable",
+							type = "toggle",
+							get = function(info)
+								return self:IsEnabled()
+							end,
+							set = function(info, key)
+								if key then
+									self:Enable()
+								else
+									self:Disable()
+								end
+								self.db.profile.enabled = key
+							end,
+							order = 2
+						},
+						pointdesc = {
+							name = "Set Where to show the duration",
+							type = "description",
+							order = 3,
+						},
+						point = {
+							name = "point",
+							type = "select",
+							order = 4,
+							values = {
+								["TOP"] = "TOP",
+								["BOTTOM"] = "BOTTOM",
+							}
+						},
+					}
+				},
+			}
+		}
+	end
+
+	self:SetEnabledState(self.db.profile.enabled)
 end
 
 local GetPoint = function(point)
-	local anchor, relative
+	local anchor, relative, mod
 	if point == "TOP" then
 		relative = "TOP"
 		anchor = "BOTTOM"
+		mod = 1
 	elseif point == "BOTTOM" then
 		relative = "BOTTOM"
 		anchor = "TOP"
+		mod = -1
 	end
-	return anchor, relative
+	return anchor, relative, mod
 end
+
+function duration:UpdateDisplay()
+	for i, buff in ipairs(bollo.buffs) do
+		if not buff.duration then break end
+		local font, size, flag = self.db.profile.font, self.db.profile.fontSize, self.db.profile.fontStyle
+		local point = self.db.profile.point
+		local x, y = self.db.profile.x, self.db.profile.y
+
+		local anchor, relative, mod = GetPoint(point)
+
+		buff.duration:SetFont(font, size, flag)
+		buff.duration:ClearAllPoints()
+		buff.duration:SetPoint(anchor, buff, relative, mod * x, y)
+	end
+	for i, buff in ipairs(bollo.debuffs) do
+		if not buff.duration then break end
+		local font, size, flag = self.db.profile.font, self.db.profile.fontSize, self.db.profile.fontStyle
+		local point = self.db.profile.point
+		local x, y = self.db.profile.x, self.db.profile.y
+
+		local anchor, relative, mod = GetPoint(point)
+
+		buff.duration:SetFont(font, size, flag)
+		buff.duration:ClearAllPoints()
+		buff.duration:SetPoint(anchor, buff, relative, mod * x, y)
+	end
+
+end
+
 
 function duration:PostCreateIcon(event, parent, button)
 	local duration = button:CreateFontString(nil, "OVERLAY")
 	local font, size, flag = self.db.profile.font, self.db.profile.fontSize, self.db.profile.fontStyle
-	local point = self.db.profile.Point
+	local point = self.db.profile.point
 	local x, y = self.db.profile.x, self.db.profile.y
 
-	local anchor, relative = GetPoint(point)
+	local anchor, relative, mod = GetPoint(point)
 
 	duration:SetFont(font, size, flag)
 	duration:ClearAllPoints()
-	duration:SetPoint(anchor, button, relative, x, y)
+	duration:SetPoint(anchor, button, relative, mod * x, y)
 	button.duration = duration
 end
 
 function duration:OnEnable()
-	self.options = {
-		name = "Duration",
-		type = "group",
-		args = {
-			general = {
-				name = "General Settings",
-				type = "group",
-				order = 1,
-				args = {
-					info = {
-						name = "Info",
-						type = "description",
-						order = 1,
-					}
-				}
-			},
-		}
-	}
 	self:OnUpdate()
 end
 
+function duration:OnDisable()
+	self.frame:SetScript("OnUpdate", nil)
+	for k, v in ipairs(bollo.buffs) do
+		v.duration:Hide()
+	end
+	for k, v in ipairs(bollo.debuffs) do
+		v.duration:Hide()
+	end
+end
+
 function duration:OnUpdate()
-	self.frame = CreateFrame("Frame")
-	local timer = 0
+	self.frame = self.frame or CreateFrame("Frame")
+	local timer = 1
 	self.frame:SetScript("OnUpdate", function(self, elapsed)
 		timer = timer + elapsed
 		if timer > 0.25 then
