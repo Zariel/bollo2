b92022b01735e54d62c76e4aee0ae4dd20979bef
Zariel <c.bannister@gmail.com>
2008-07-12 20:57:18 +0100

    Init module options too

--- modules/config.lua
+++ modules/config.lua
@@ -21,12 +21,6 @@ local InitCore = function()
 				general = {
 					order = 1,
 					type = "group",
-					get = optGet,
-					set = function(info, val)
-						local key = info[# info]
-						bollo.db.profile[key] = val
-						bollo:UpdateSettings()
-					end,
 					name = "General Settings",
 					args = {
 						desc = {
@@ -34,39 +28,64 @@ local InitCore = function()
 							type = "description",
 							name = "Bollo displays Buffs and Debuffs",
 						},
-						size = {
+						buff = {
 							order = 2,
-							name = "Size",
-							type = "range",
-							min = 10,
-							max = 50,
-							step = 1,
+							guiInline = true,
+							type = "group",
+							get = optGet,
+							set = function(info, val)
+								local key = info[# info]
+								bollo.db.profile.buff[key] = val
+								bollo:UpdateSettings(bollo.buffs)
+							end,
+							get = function(info)
+								local key = info[# info]
+								return bollo.db.profile.buff[key]
+							end,
+							name = "Buff Settings",
+							args = {
+								desc = {
+									order = 1,
+									type = "description",
+									name = "Settings for Buff display",
+								},
+								size = {
+									order = 2,
+									name = "Size",
+									type = "range",
+									min = 10,
+									max = 50,
+									step = 1,
+								},
+								spacing = {
+									order = 3,
+									name = "Spacing",
+									type = "range",
+									min = -20,
+									max = 20,
+									step = 1,
+									},
+								},
+							},
 						},
-						spacing = {
-							order = 3,
-							name = "Spacing",
-							type = "range",
-							min = -20,
-							max = 20,
-							step = 1,
-						},
-					},
-				},
-			},
-		}
+					}
+				}
+			}
 	end
 	return defaults
 end
 
 
 function conf:OnEnable()
-	for name, module in bollo:IterateModules() do
-		if module.options then
-		end
-	end
 
 	LibStub("AceConfigRegistry-3.0"):RegisterOptionsTable("Bollo", InitCore)
 	bollo.options = LibStub("AceConfigDialog-3.0"):AddToBlizOptions("Bollo", nil, nil, "general")
+
+	for name, module in bollo:IterateModules() do
+		if module.options then
+			LibStub("AceConfigRegistry-3.0"):RegisterOptionsTable(tostring(module), module.options)
+			module.bliz = LibStub("AceConfigDialog-3.0"):AddToBlizOptions(tostring(module), name, "Bollo", "general")
+		end
+	end
+
 end
-
-
