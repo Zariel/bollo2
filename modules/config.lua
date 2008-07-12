fd470aa1b6667f00409551169bc90158f130ecfd
Zariel <c.bannister@gmail.com>
2008-07-12 19:58:31 +0100

    Start hashing out basic config

--- modules/config.lua
+++ modules/config.lua
@@ -1,9 +1,72 @@
-local bollo = LibStub("AceAddon-3.0"):GetAddon(bollo)
-local conf = bollo:NewModule("Config")
+--[[
+	config.lua
+		Adds module configs to interface panels
+]]
+
+local bollo = LibStub("AceAddon-3.0"):GetAddon("Bollo")
+local conf = bollo:NewModule("Config", "AceConsole-3.0")
+
+local optGet = function(info)
+	local key = info[#info]
+	return bollo.db.profile[key]
+end
+
+local defaults
+local InitCore = function()
+	if not defaults then
+		defaults = {
+			type = "group",
+			name = "Bollo",
+			args = {
+				general = {
+					order = 1,
+					type = "group",
+					get = optGet,
+					set = function(info, val)
+						local key = info[# info]
+						bollo.db.profile[key] = val
+						bollo:UpdateSettings()
+					end,
+					name = "General Settings",
+					args = {
+						desc = {
+							order = 1,
+							type = "description",
+							name = "Bollo displays Buffs and Debuffs",
+						},
+						size = {
+							order = 2,
+							name = "Size",
+							type = "range",
+							min = 10,
+							max = 50,
+							step = 1,
+						},
+						spacing = {
+							order = 3,
+							name = "Spacing",
+							type = "range",
+							min = -20,
+							max = 20,
+							step = 1,
+						},
+					},
+				},
+			},
+		}
+	end
+	return defaults
+end
+
 
 function conf:OnEnable()
+	for name, module in bollo:IterateModules() do
+		if module.options then
+		end
+	end
+
+	LibStub("AceConfigRegistry-3.0"):RegisterOptionsTable("Bollo", InitCore)
+	bollo.options = LibStub("AceConfigDialog-3.0"):AddToBlizOptions("Bollo", nil, nil, "general")
 end
 
-function bollo:OnModuleCreated(module)
-	self:Print(module)
-end
+
