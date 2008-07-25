5a40d52db23bf5581f268213d96e23235e76210a
Zariel <c.bannister@gmail.com>
2008-07-25 15:30:20 +0100

    Added options and general fixes

--- modules/totems.lua
+++ modules/totems.lua
@@ -19,11 +19,51 @@ function Totem:OnInitialize()
 				["y"] = 0,
 				["height"] = 100,
 				["width"] = 350,
-				["rowSpace"] = 20,= 200,
+				["rowSpace"] = 20,
+				["Name"] = {
+					["Description"] = "Shows truncated names of buffs",
+					["font"] = STANDARD_TEXT_FONT,
+					["fontStyle"] = "OUTLINE",
+					["fontSize"] = 9,
+					["x"] = 0,
+					["y"] = 0,
+					["point"] = "BOTTOM",
+					["color"] = {
+						r = 1,
+						g = 1,
+						b = 1,
+						a = 1,
+					},
+					["enabled"] = true,
+				},
+				["Duration"] = {
+					["Description"] = "Show buff durations",
+					["point"] = "TOP",
+					["font"] = STANDARD_TEXT_FONT,
+					["fontSize"] = 9,
+					["fontStyle"] = "OUTLINE",
+					["x"] = 0,
+					["y"] = 0,
+					["format"] = "M:SS",
+					["color"] = {
+						r = 1,
+						g = 1,
+						b = 1,
+						a = 1,
+					}
+				}
 			}
 		}
 	}
-	self.db = bollo.db:RegisterNameSpace("Totems", defaults)
+	self.db = bollo.db:RegisterNamespace("Totems", defaults)
+
+	self.options = {
+		name = "Totems",
+		type = "group",
+		order = 1,
+		args = {},
+	}
+
 end
 
 function Totem:OnEnable()
@@ -31,8 +71,8 @@ function Totem:OnEnable()
 
 	if not bollo.icons.totem.bg then
 		local bg = CreateFrame("Frame", nil, UIParent)
-		bg:SetWidth(Weapon.db.profile.totem.width)
-		bg:SetHeight(Weapon.db.profile.totem.height)
+		bg:SetWidth(Totem.db.profile.totem.width)
+		bg:SetHeight(Totem.db.profile.totem.height)
 
 		bg:SetBackdrop({
 			bgFile = "Interface\\ChatFrame\\ChatFrameBackground", tile = true, tileSize = 16,
@@ -52,20 +92,27 @@ function Totem:OnEnable()
 
 		bg:SetScript("OnMouseUp", function(self, button)
 			local x, y = self:GetLeft(), self:GetTop()
-			Weapon.db.profile.totem.x, Weapon.db.profile.totem.y = x, y
+			Totem.db.profile.totem.x, Totem.db.profile.totem.y = x, y
 			return self:StopMovingOrSizing()
 		end)
 
-		bg:SetPoint("TOPLEFT", UIParent, "BOTTOMLEFT", Weapon.db.profile.totem.x, Weapon.db.profile.totem.y)
+		bg:SetPoint("TOPLEFT", UIParent, "BOTTOMLEFT", Totem.db.profile.totem.x, Totem.db.profile.totem.y)
 
 		bollo.icons.totem.bg = bg
 	end
 
+	bollo:GetModule("Config"):AddChildOpts("totem", Totem.db.profile.totem, Totem)
+
+	for k, v in bollo:IterateModules() do
+		if v.AddOptions and self.db.profile.totem[k] then
+			v:AddOptions("totem", self.db.profile.totem[k], self)
+		end
+	end
 	self:RegisterEvent("PLAYER_TOTEM_UPDATE")
 end
 
 function Totem:SetupIcons()
-	if bollo.icons["totem"] then
+	if bollo.icons.totem then
 		return
 	else
 		bollo.icons.totem = setmetatable({}, {__tostring = function() return "totem" end})
@@ -98,16 +145,17 @@ function Totem:SetupIcons()
 				GameTooltip:SetTotem(self:GetID())
 			end
 		end)
+		b:Hide()
 	end
 end
 
 local totems = {}
 
 local _, name, start, duration
-function Totem:PLAYER_TOTEM_UDPDATE()
+function Totem:PLAYER_TOTEM_UPDATE()
+	-- Madness
 	for i = 1, 4 do
 		_, name, startTime, duration, icon = GetTotemInfo(i)
-		-- Madness
 		if (not totems[i] or totems[i] ~= name) and duration > 0 then
 			local buff = icons[i]
 			buff.icon:SetTexture(icon)
@@ -117,6 +165,7 @@ function Totem:PLAYER_TOTEM_UDPDATE()
 			icons[i]:Hide()
 			totems[i] = nil
 		end
+		bollo.events:Fire("PostSetBuff", icons[i], i, nil)
 	end
 
 	local offset = 0
@@ -130,6 +179,7 @@ function Totem:PLAYER_TOTEM_UDPDATE()
 	local anchor = growthx > 0 and "LEFT" or "RIGHT"
 	local relative = growthy  > 0 and "BOTTOM" or "TOP"
 	local point = relative .. anchor
+
 	for i, buff in ipairs(icons) do
 		if buff:IsShown() then
 			if offset == perCol then
@@ -137,8 +187,7 @@ function Totem:PLAYER_TOTEM_UDPDATE()
 				offset = 0
 			end
 
-			buff:SetPoint(point, icons.bg, point, (offset * (size + self.db.profile[name].spacing) * growthx), (rows * (size + rowSpace) * growthy))
-			self.events:Fire("UpdateIconPosition", i, buff, icons)
+			buff:SetPoint(point, icons.bg, point, (offset * (size + self.db.profile.totem.spacing) * growthx), (rows * (size + rowSpace) * growthy))
 			offset = offset + 1
 		end
 	end
