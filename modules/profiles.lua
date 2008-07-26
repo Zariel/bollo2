976a0441f46d984d1fb1e58b3c1c0db1c4a18742
Zariel <c.bannister@gmail.com>
2008-07-26 17:43:44 +0100

    Remove v1

--- modules/profiles.lua
+++
@@ -1,9 +0,0 @@
-local bollo = LibStub("AceAddon-3.0"):GetAddon("Bollo")
-local profiles = bollo:NewModule("Profiles")
-
-function profiles:OnInitialize()
-	if not self.options then
-		self.options =  LibStub("AceDBOptions-3.0"):GetOptionsTable(bollo.db)
-	end
-	bollo:AddOptions(self)
-end
