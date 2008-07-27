6c190522d17a03c2308c65db6e0f4e2fbc3a3cdf
Zariel <c.bannister@gmail.com>
2008-07-27 23:00:06 +0100

    Move class into core

--- core.lua
+++ core.lua
@@ -1,1 +1,28 @@
-local Bollo = LibStub("AceAddon-3.0"):NewAddon("Bollo", "AceEvent-3.0")
+local class
+do
+	local new = function(c)
+		local o = {}
+		local mt = {__index = c}
+		setmetatable(o, mt)
+		return o
+	end
+
+	class = function(parent)
+		local c = { new = new }
+		local mt = {__index = parent}
+		setmetatable(c, mt)
+		return c
+	end
+end
+
+local Bollo = LibStub("AceAddon-3.0"):NewAddon("Bollo")
+Bollo.class = class
+
+function Bollo:OnEnable()
+	local bf = _G["BuffFrame"]
+	bf:UnregisterAllEvents()
+	bf:Hide()
+	bf:SetScript("OnUpdate", nil)
+	bf:SetScript("OnEvent", nil)
+	_G.BuffButton_OnUpdate = nil
+end
