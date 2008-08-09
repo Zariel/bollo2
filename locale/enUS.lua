0293d949da48086a4fc736c8f49b90d28c71c704
Zariel <c.bannister@gmail.com>
2008-08-09 20:36:59 +0100

    v2

--- locale/enUS.lua
+++ locale/enUS.lua
@@ -1,4 +1,4 @@
-local Bollo = LibStub("AceAddon-3.0"):GetAddon("Bollo")
+local Bollo = LibStub("AceAddon-3.0"):GetAddon("Bollo2")
 local L = LibStub("AceLocale-3.0"):NewLocale("Bollo2", "enUS", true)
 
 L["Lock Background"] = true
